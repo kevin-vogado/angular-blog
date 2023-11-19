3a9fee7e2b3e4434b93a6a507ce1fc5b136cab48
kevin-vogado <krosstt14@gmail.com>
2023-11-19 18:00:43 -0300

    content created with repository

--- angular-blog/src/app/components/small-card/small-card.component.ts
+++ angular-blog/src/app/components/small-card/small-card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  photoSmall: string = '';
+  @Input()
+  smallTitle: string = '';
+  @Input()
+  Id: string = '0';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    id: "1",
+    photo: 'https://hermes.dio.me/articles/cover/9ab7f864-dc92-4157-9c09-deb776f83da6.jpg',
+    title: 'React vs Angular',
+    text: 'Angular is a full-fledged framework, while React is a JavaScript library. Therefore, React has to be paired with a framework to build fast.',
+  },
+  {
+    id: "2",
+    photo: 'https://coreui.io/angular/docs/assets/img/angular.jpg',
+    title: 'Angular, a inovação!',
+    text: 'O Angular vai mudar o mundo! E vamos estar lá para presenciar esta mudança, pois o Angular vem surpreendendo com novas atualizações.',
+  },
+  {
+    id: "3",
+    photo: 'https://www.freecodecamp.org/portuguese/news/content/images/2022/10/img.png',
+    title: 'Tudo o que o Angular tem a oferecer!',
+    text: 'O Angular está mudando a forma como os frameworks atuam no mundo do desenvolvimento, venha ver e experimentar um framework consolidado!'
+  },
+  {
+    id: "4",
+    photo: 'https://miro.medium.com/v2/resize:fit:801/0*lX6dpmBTb2eRxu7A',
+    title: 'Next.js vs Angular é o assunto do momento da internet, você sabe de que lado está?',
+    text: 'Sabemos o poder de cada framework, mas chegou a hora de tirarmos as diferenças e darmos a "coroa" ao rei. Vamos nessa explorar o poder de cada um!'
+  }
+];
